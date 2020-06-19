5074eb12c8eb36fd1c844ed0efe998653ad358c4
marioelias98 <67127473+marioelias98@users.noreply.github.com>
2020-06-18 19:17:41 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+var menuP = document.getElementById("menu-p");
+var nav = document.getElementById('nav');
+
+menuP.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
+
+                                  
+                                  
+
+
+
+
+ 
